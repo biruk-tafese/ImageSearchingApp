1a76edd656b399e6e1017852594e3c3e3387ce51
Biruk Tafesse <98220455+biruk-tafese@users.noreply.github.com>
2024-05-02 20:24:58 +0000

    pages

--- homegate/src/App.js
+++ homegate/src/App.js
@@ -1,14 +1,20 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import Homegate from './Homegate';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Hello Biruk! Welcome to Homegate!
-        </p>
-      </header>
-    </div>
+    <Router>
+       <Navbar />
+       <Routes>
+        <Route path="/" element={<Homegate />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ homegate/src/Homegate.jsx
@@ -0,0 +1,13 @@
+
+function Homegate() {
+  return (
+     <>
+    <div>
+      <h1>Welcome to Homegate</h1>
+      <p>Homegate is a platform for buying and selling homes.</p>
+    </div>
+    </>
+  );
+}
+
+export default Homegate;
